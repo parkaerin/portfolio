ccdf0bc61a91633fef00c32e59c699e6299c79a4
love90719@naver.com <love90719@naver.com>
2021-08-23 01:19:57 +0900

    띄어쓰기 수정

--- src/portfolio.js
+++ src/portfolio.js
@@ -37,9 +37,9 @@ const skillsSection = {
         emoji("⚡ Webpack 및 map/chart/editor/MobX 등 다양한 라이브러리 경험"),
         emoji("⚡ 대량의 데이터(초당 수천 건)가 실시간으로 업데이트 되는 기기 모니터링 Web Application 화면 개발 경험"),
         emoji("⚡ pdf,MS office 기반 문서 검색을 주기능으로 하는 검색 포털 사이트 개발 경험"),
-        emoji("⚡ 2년 간 개발자 간 소통을 극대화하고 항상 최선의 코드를 작성하는 Test-Driven Development, Pair Prog ramming 진행"),
+        emoji("⚡ 2년 간 개발자 간 소통을 극대화하고 항상 최선의 코드를 작성하는 Test-Driven Development, Pair Programming 진행"),
         emoji("⚡ RESTful API 기반의 Microservice Architecture 경험 및 AWS/Azure Cloud 인프라 기반 CI/CD 경험"),
-        emoji("⚡ Java/C++ 개발 경험을 보유하여 새로운 언어를 빠르게 습득하고, Front-End 개발 시에도 다각적인 접근 가 능")
+        emoji("⚡ Java/C++ 개발 경험을 보유하여 새로운 언어를 빠르게 습득하고, Front-End 개발 시에도 다각적인 접근 가능")
     ],
 
     /* Make Sure You include correct Font Awesome Classname to view your icon
